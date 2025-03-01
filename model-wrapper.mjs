fee76cf1e235ba45d041a8b911f9c3d4569bd94b
Dmitry Zlygin <dzlygin@gmail.com>
2025-02-28 23:57:26 -0500

    added more models

--- model-wrapper.mjs
+++ model-wrapper.mjs
@@ -1,75 +1,79 @@
 import * as ort from './ort-dist/ort.all.min.mjs';
 import ImageConverter from './image-converter.mjs';
 
-export const MODEL_HEIGHT = 128;
-export const MODEL_WIDTH = 128;
-const MODEL_CHANNELS = 3;
-export const MODEL_SCALE = 4;
 export const MIN_TILE_OVERLAP = 8;
 
 export default class ModelWrapper {
-    constructor(width = 320, height = 240) {
-        this._initModel();
+    constructor(width, height, model) {
+        this._model = model;
+        this._initModel(model);
 
         this.width = width;
         this.height = height;
 
+        if (model.height < height || model.width < width) {
+            this._tiled = true;
+            this.layout = this.makeLayout(model);
+        } else {
+            this._tiled = false;
+        }
+
         this.converter = new ImageConverter();
 
-        this.inputFrame = this.converter.getInputBufferView(width, height, MODEL_CHANNELS);
-        this.inputBuffer = this.converter.getOutputBufferView(MODEL_WIDTH, MODEL_HEIGHT);
-        this.inputTensor = new ort.Tensor(this.inputBuffer, [1, MODEL_CHANNELS, MODEL_HEIGHT, MODEL_WIDTH]);
+        this.inputFrame = this.converter.getInputBufferView(width, height, model.channels);
+        this.inputBuffer = this.converter.getOutputBufferView(model.width, model.height);
+        this.inputTensor = new ort.Tensor(this.inputBuffer, [1, model.channels, model.height, model.width]);
 
-        this.outputBuffer = new Uint8ClampedArray((MODEL_WIDTH * MODEL_SCALE) * (MODEL_HEIGHT * MODEL_SCALE) * MODEL_CHANNELS);
-
-        this.makeLayout();
+        this.outputBuffer = new Uint8ClampedArray((model.width * model.scale) * (model.height * model.scale) * model.channels);
     }
 
-    _initModel() {
-        this.modelPromise = ort.InferenceSession.create('SESR-M5.onnx', {
+    _initModel(model) {
+        this.modelPromise = ort.InferenceSession.create(model.file, {
             // executionProviders: ['webgl'], // incorrect results
             // executionProviders: ['wasm'],
             executionProviders: ['webgpu'], // the most performant
             preferredOutputLocation: 'cpu-pinned',
+            ...model.additionalParameters,
         });
     }
 
-    makeLayout() {
-        this.tilesH = Math.ceil((this.width - MIN_TILE_OVERLAP) / (MODEL_WIDTH - MIN_TILE_OVERLAP));
-        this.tilesV = Math.ceil((this.height - MIN_TILE_OVERLAP) / (MODEL_HEIGHT - MIN_TILE_OVERLAP));
+    makeLayout(model) {
+        this.tilesH = Math.ceil((this.width - MIN_TILE_OVERLAP) / (model.width - MIN_TILE_OVERLAP));
+        this.tilesV = Math.ceil((this.height - MIN_TILE_OVERLAP) / (model.height - MIN_TILE_OVERLAP));
 
-        this.layout = [];
+        const layout = [];
         for (let i = 0; i < this.tilesV; i++) {
             for (let j = 0; j < this.tilesH; j++) {
                 const tile = {
-                    x: j * (MODEL_WIDTH - MIN_TILE_OVERLAP),
+                    x: j * (model.width - MIN_TILE_OVERLAP),
                     lastInRow: j === this.tilesH - 1,
-                    y: i * (MODEL_HEIGHT - MIN_TILE_OVERLAP),
+                    y: i * (model.height - MIN_TILE_OVERLAP),
                     lastInColumn: i === this.tilesV - 1,
                 };
                 if (j > 0 && tile.lastInRow) {
-                    tile.x = this.width - MODEL_WIDTH;
+                    tile.x = this.width - model.width;
                 }
                 if (i > 0 && tile.lastInColumn) {
-                    tile.y = this.height - MODEL_HEIGHT;
+                    tile.y = this.height - model.height;
                 }
-                this.layout.push(tile);
+                layout.push(tile);
             }
         }
+
+        return layout;
     }
 
     async processFrame() {
-        if (this.layout.length === 1) {
-            this.converter.convertI420TileToCHW(0, 0, MODEL_WIDTH, MODEL_HEIGHT, this.width, this.height);
-            // this.inputBuffer.fill(0, MODEL_WIDTH * MODEL_HEIGHT);
+        const model = this._model;
+        if (!this._tiled) {
+            this.converter.convertI420ToCHW(this.width, this.height);
 
-            return [this.prepareOutput(await this.modelPromise.then(model => model.run({ image: this.inputTensor })))];
+            return [this.prepareOutput(await this.modelPromise.then(model => model.run({ [this._model.inputName]: this.inputTensor })))];
         }
 
         const frames = [];
         for(const tile of this.layout) {
-            this.converter.convertI420TileToCHW(tile.x, tile.y, MODEL_WIDTH, MODEL_HEIGHT, this.width, this.height);
-            this.inputBuffer.fill(0.5, MODEL_WIDTH * MODEL_HEIGHT); // Forced grayscale, needs at least bilinear upscaling for UV
+            this.converter.convertI420TileToCHW(tile.x, tile.y, model.width, model.height, this.width, this.height);
             const result = await this.modelPromise.then(model => model.run({ image: this.inputTensor }));
             frames.push(this.prepareOutput(result));
         }
@@ -78,7 +82,8 @@ export default class ModelWrapper {
     }
 
     prepareOutput(result) {
-        const { data } = result.upscaled_image;
+        const model = this._model;
+        const { data } = result[model.outputName];
         let ptr = 0;
         const ln = this.outputBuffer.length;
         while (ptr < ln) {
@@ -87,8 +92,8 @@ export default class ModelWrapper {
         }
 
         return new VideoFrame(this.outputBuffer, {
-            codedWidth: MODEL_WIDTH * 4,
-            codedHeight: MODEL_HEIGHT * 4,
+            codedWidth: model.width * model.scale,
+            codedHeight: model.height * model.scale,
             format: 'I444',
             timestamp: Date.now(),
         });
---
+++ models.mjs
@@ -0,0 +1,109 @@
+export const sesrM5 = {
+    file: 'SESR-M5.onnx',
+    width: 128,
+    height: 128,
+    channels: 3,
+    scale: 4,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const quickSr4x180 = {
+    file: 'qai-hub/quicksrnetsmall-4x-180p.onnx',
+    width: 320,
+    height: 180,
+    channels: 3,
+    scale: 4,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const quickSr2x360 = {
+    file: 'qai-hub/quicksrnetsmall-2x-360p.onnx',
+    width: 640,
+    height: 360,
+    channels: 3,
+    scale: 2,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const quickSr2x1080 = {
+    file: 'qai-hub/quicksrnetsmall-2x-1080p.onnx',
+    width: 1920,
+    height: 1080,
+    channels: 3,
+    scale: 2,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const xlsr4x180 = {
+    file: 'qai-hub/xlsr-4x-180p.onnx',
+    width: 320,
+    height: 180,
+    channels: 3,
+    scale: 4,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const xlsr2x360 = {
+    file: 'qai-hub/xlsr-2x-360p.onnx',
+    width: 640,
+    height: 360,
+    channels: 3,
+    scale: 2,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const xlsr2x1080 = {
+    file: 'qai-hub/xlsr-2x-1080p.onnx',
+    width: 1920,
+    height: 1080,
+    channels: 3,
+    scale: 2,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const sesr_m5_4x180 = {
+    file: 'qai-hub/sesr_m5-4x-180p.onnx',
+    width: 320,
+    height: 180,
+    channels: 3,
+    scale: 4,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const sesr_m5_2x360 = {
+    file: 'qai-hub/sesr_m5-2x-360p.onnx',
+    width: 640,
+    height: 360,
+    channels: 3,
+    scale: 2,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
+
+export const sesr_m5_2x1080 = {
+    file: 'qai-hub/sesr_m5-2x-1080p.onnx',
+    width: 1920,
+    height: 1080,
+    channels: 3,
+    scale: 2,
+    additionalParameters: {},
+    inputName: 'image',
+    outputName: 'upscaled_image',
+};
